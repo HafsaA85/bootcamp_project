363fed5c02b064dfc955d3fba374f880c07f2804
HafsaA85 <hafsaadeel4@gmail.com>
2025-10-07 14:34:10 +0100

    Call user name, email and password set in Heroku if not set

--- clients/management/commands/create_superuser.py
+++ clients/management/commands/create_superuser.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+import os
 
-#custom Django management command in your project that creates a superuser programmatically
 class Command(BaseCommand):
+    help = 'Create superuser from environment variables if not exists'
+
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')
-            self.stdout.write('Superuser created')
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+        # Check if environment variables are provided
+        if not all([username, email, password]):
+            self.stdout.write('One or more environment variables are missing.')
+            return
+
+        # Check if superuser already exists
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(f'Superuser "{username}" created.')
         else:
-            self.stdout.write('Superuser already exists')
+            self.stdout.write(f'Superuser "{username}" already exists.')
