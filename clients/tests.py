803d86d54bd8a0be5ea8be212bee566e79ff1ff6
HafsaA85 <hafsaadeel4@gmail.com>
2025-10-08 12:08:36 +0100

    Only Admin can see Admin button

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Client, Appointment
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class ClientModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.client_profile = Client.objects.create(user=self.user, phone='1234567890')
+
+    def test_client_str(self):
+        self.assertEqual(str(self.client_profile), 'testuser')
